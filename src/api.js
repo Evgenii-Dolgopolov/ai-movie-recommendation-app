e7719ce69f3cff685d44446ea575059360f252fd
Evgenii-Dolgopolov <do.evgenii@gmail.com>
2024-05-30 12:47:59 +0700

    setup supabase

--- src/api.js
+++ src/api.js
@@ -1,12 +1,22 @@
 import OpenAI from "openai"
+import { createClient } from "@supabase/supabase-js"
 
-/** Ensure the OpenAI API key is available and correctly configured */
+// Ensure the OpenAI API key is available and correctly configured
 if (!import.meta.env.VITE_OPENAI_API_KEY) {
   throw new Error("OpenAI API key is missing or invalid.")
 }
 
-/** OpenAI config */
-export default new OpenAI({
+// OpenAI config
+const openai = new OpenAI({
   apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
 })
+export default openai
+
+// Supabase
+const privateKey = import.meta.env.VITE_SUPABASE_API_KEY
+if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`)
+const url = import.meta.env.VITE_SUPABASE_URL
+if (!url) throw new Error(`Expected env var SUPABASE_URL`)
+
+export const supabase = createClient(url, privateKey)
